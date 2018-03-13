ed6ba4bee7245da61932d2e601a2ba982b7f881b
jazzhong1 <jazzhong1@naver.com>
2018-03-13 13:10:29 +0900

    Initial commit

---
+++ src/com/iu/db1/InsertTest.java
@@ -0,0 +1,14 @@
+package com.iu.db1;
+
+public class InsertTest {
+
+	public InsertTest() {
+		// TODO 자동 생성된 생성자 스텁
+	}
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+
+	}
+
+}
